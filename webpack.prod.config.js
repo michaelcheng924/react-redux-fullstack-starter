3b3ef106fecf52e164e7fa20230de27ee9ab648c
michaelcheng429 <cheng@csdisco.com>
2016-01-09 16:15:06 -0600

    add webpack and babel-loader

---
+++ webpack.prod.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './shared/components',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'shared'],
+        extensions: ['', '.js', '.jsx', 'scss']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
