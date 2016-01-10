0354b55b8e6ce1edf3dd6d2d0a8167735522d0c7
michaelcheng429 <cheng@csdisco.com>
2016-01-09 18:47:59 -0600

    make app isomorphic, routing server side

--- server.jsx
+++ server.jsx
@@ -1,8 +1,50 @@
 import express from 'express';
 import path from 'path';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { RoutingContext, match } from 'react-router';
+import createLocation from 'history/lib/createLocation';
+import routes from 'routes';
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+    const location = createLocation(req.url);
+
+    match({ routes, location }, (err, redirectLocation, renderProps) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).end('Internal server error');
+        }
+
+        if (!renderProps) {
+            return res.status(404).end('Not found.');
+        }
+
+        const InitialComponent = (
+            <RoutingContext {...renderProps} />
+        );
+
+        const componentHTML = renderToString(InitialComponent);
+
+        const HTML = `
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Best ACT Prep</title>
+                </head>
+                <body>
+                    <div id="app">${componentHTML}</div>
+                    <script src="/bundle.js"></script>
+                </body>
+            </html>
+        `;
+
+        res.end(HTML);
+    });
+});
 
 export default app;
