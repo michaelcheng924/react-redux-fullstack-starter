07589aba343d2809f09a2d9a2f33958726228797
michaelcheng429 <cheng@csdisco.com>
2016-01-10 23:18:40 -0600

    commit

---
+++ generator-react-redux-fullstack/app/index.js
@@ -0,0 +1,11 @@
+var generators = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+module.exports = generators.Base.extend({
+    prompting: function() {
+        this.log(yosay(
+            'Welcome to the ' + chalk.green('React Redux Fullstack Starter') + ' generator!'
+        ));
+    }
+});
