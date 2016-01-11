a461564cb18eb054787c6e72ff160bc5ef1aaf88
michaelcheng429 <cheng@csdisco.com>
2016-01-10 23:33:42 -0600

    add write

--- generator-react-redux-fullstack/app/index.js
+++ generator-react-redux-fullstack/app/index.js
@@ -1,11 +1,38 @@
+var path = require('path');
 var generators = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
+var glob = require('glob');
 
 module.exports = generators.Base.extend({
+    initializing: function () {
+        this.sourceRoot(path.join(this.sourceRoot(), '../../react-redux-fullstack-starter'));
+    },
+
     prompting: function() {
         this.log(yosay(
             'Welcome to the ' + chalk.green('React Redux Fullstack Starter') + ' generator!'
         ));
+    },
+
+    writing: function() {
+        var done = this.async();
+        glob('**/*', { cwd: this.sourceRoot(), dot: true }, function (err, files) {
+            if (err) {
+                this.log('Error:', err.message);
+                return done();
+            }
+            files.forEach(function (file) {
+                var dest = file;
+                if (file === 'npmignore') {
+                    dest = '.' + file;
+                }
+                this.fs.copy(
+                    this.templatePath(file),
+                    this.destinationPath(dest)
+                );
+            }, this);
+            done();
+        }.bind(this));
     }
 });
