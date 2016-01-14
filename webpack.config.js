827799cfad9f0dae4177930d962d4ccb9f8e33f0
michaelcheng429 <cheng@csdisco.com>
2016-01-14 14:37:07 -0600

    add image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
                     'autoprefixer?browsers=last 3 versions',
                     'sass?outputStyle=expanded'
                 ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'url?limit=8192',
+                    'img'
+                ]
             }
         ]
     },
