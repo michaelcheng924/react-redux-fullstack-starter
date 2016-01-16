d36e213ab4e91ba46c1fc5776b2201a43b46b343
michaelcheng429 <cheng@csdisco.com>
2016-01-16 17:08:57 -0600

    add to about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,11 +13,33 @@ export default class About extends React.Component {
                 This starter includes the following (among other smaller dependencies):
 
                 <ul>
-                    <li><strong>React + react-router</strong></li>
-                    <li><strong>Redux + immutable</strong></li>
-                    <li><strong>Webpack</strong></li>
-                    <li><strong>Mocha + Chai (testing)</strong></li>
+                    <li><strong>React</strong> - Great framework for building scalable, performant web apps</li>
+                    <li><strong>react-router</strong> - Routing library for React</li>
+                    <li><strong>Redux</strong> - Great philosophy for managing data, data flow, and state with a single state tree</li>
+                    <li><strong>Immutable</strong> - Prevents mutation of state. Always return brand new states when state updates.</li>
+                    <li><strong>Webpack</strong> - Amazing module bundler that basically replaces Grunt and Gulp</li>
+                    <li><strong>webpack-dev-server + react-hot-loader</strong> - Makes development a joy with easy testing and live reload</li>
+                    <li><strong>Node + Express</strong> - Asynchronous JavaScript back-end. Node is fast, and Express makes writing Node easier and more fun.</li>
+                    <li><strong>Mocha + Chai (testing)</strong> - Great testing framework and assertion library</li>
                 </ul>
+
+                <h3>Highlights</h3>
+                Why use this starter/stack?
+
+                <ul>
+                    <li><strong>React + Redux is awesome</strong> - React and Redux go together beautifully. Uni-directional data flow. Have a single immutable state tree for your app, pass what you need into top level components, and then let these props trickle into lower level, "dumb" components. Know exactly where all data-manipulation happens, and prevent unintended side effects.</li>
+                    <li><strong>Isomorphic</strong> - Server side rendering for improved load times and easy server-side routing</li>
+                    <li><strong>Save hours of setting up the stack above to work together</strong> - One of the primary obstacles to using this great stack is how long it takes to get set up. Get all of the basics of this stack, without a ton of fancy junk you don't want.</li>
+                    <li><strong>Comments and examples to help you learn and get started</strong> - There is some complexity to this stack. That's why there is so much information here about how to use this stack and this starter.</li>
+                    <li><strong>Easy deployment to Heroku</strong> - Three simple steps (after downloading the Heroku toolbelt and doing <code>heroku login</code>: 1) <code>git init</code> 2) <code>heroku create <em>app-name</em></code>, 2) <code>git push heroku master</code></li>
+                </ul>
+
+                <h3>Getting Started</h3>
+                Here's how you get started:
+
+                <ol>
+                    <li><strong>Get the repository</strong> - </li>
+                </ol>
             </div>
         );
     }
