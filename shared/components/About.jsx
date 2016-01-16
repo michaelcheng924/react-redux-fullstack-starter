74a51eafa5801ba34ca99ce026cea26bae351d1e
michaelcheng429 <cheng@csdisco.com>
2016-01-16 13:29:30 -0600

    add about information

--- shared/components/About.jsx
+++ shared/components/About.jsx
@@ -4,8 +4,20 @@ import { Link } from 'react-router';
 export default class About extends React.Component {
     render() {
         return (
-            <div>
-                <h2>About</h2>
+            <div className="about">
+                <h2 className="about__heading">About</h2>
+
+                <div>Welcome to the React Redux Fullstack Starter! To help get you started, here is an overview of what's contained here (this information is also available at the <a href="http://github.com/michaelcheng429/react-redux-fullstack-starter" target="_blank">GitHub repository</a>:</div>
+
+                <h3>Stack</h3>
+                This starter includes the following (among other smaller dependencies):
+
+                <ul>
+                    <li><strong>React + react-router</strong></li>
+                    <li><strong>Redux + immutable</strong></li>
+                    <li><strong>Webpack</strong></li>
+                    <li><strong>Mocha + Chai (testing)</strong></li>
+                </ul>
             </div>
         );
     }
