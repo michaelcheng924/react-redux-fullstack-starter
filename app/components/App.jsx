90c8c5d1b00c909f8537e9634fdadb19da6414bc
michaelcheng429 <cheng@csdisco.com>
2017-04-01 22:16:42 -0500

    fix nav home link name

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -8,7 +8,7 @@ const App = createClass({
         return (
             <div>
                 <nav className="main-nav">
-                    <Link className="main-nav__link" to="/">Company Name</Link>
+                    <Link className="main-nav__link" to="/">App Name</Link>
                     <div className="main-nav__menu-container">
                         <Link className="main-nav__link" to="/">Home</Link>
                         <Link className="main-nav__link" to="about">About (Routing Example)</Link>
